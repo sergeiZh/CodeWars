63c551d4b5bbf3d4d10ca5c08ecb163b752fafaa
Siarhei Zhabrykau <siarhei_zhabrykau@epam.com>
2020-09-28 10:16:41 +0300

    Kata simple username validation, complexity 8, regexp - https://www.codewars.com/kata/simple-validation-of-a-username-with-regex

--- javaScriptCodeWars/test/regexp/eight/SimpleValidationOfUsername.test.js
+++ javaScriptCodeWars/test/regexp/eight/SimpleValidationOfUsername.test.js
@@ -1,13 +1,13 @@
 const {validateUsr} = require('../../../main/regexp/eight/SimpleValidationOfUsername');
 
-describe('', () => {
-    test('', () => {
+describe('User name should contain lower case latin letters, digits and underscore', () => {
+    test('Happy path', () => {
        expect(validateUsr('asddsa')).toBeTruthy();
        expect(validateUsr('____')).toBeTruthy();
        expect(validateUsr('asd43_34')).toBeTruthy();
        expect(validateUsr('p1pp1')).toBeTruthy();
     });
-    test('', () => {
+    test('Incorrect user name function should return false', () => {
         expect(validateUsr('a')).toBeFalsy();
         expect(validateUsr('Hass')).toBeFalsy();
         expect(validateUsr('Hasd_12assssssasasasasasaasasasasas')).toBeFalsy();
